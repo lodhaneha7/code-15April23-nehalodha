9f68e17319df1cdbcb310318bb0bec141e4b27c0
lodhaneha7 <nhlodha7@gmail.com>
2023-04-17 20:51:29 +0530

    API calls when run the application (using 'npm run start')

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { Logger } from '@nestjs/common';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -11,7 +12,15 @@ async function bootstrap() {
   });
   
   app.enableCors();
-  await app.listen(5000); 
+  await app.listen(5000);
+   const apiService = app.get(AppService);
+   try {
+     await apiService.calculateHealthParams();
+     //console.log('API call succeeded on startup');
+   } catch (error) {
+     console.log('API call failed on startup:', error);
+   }
+ 
 }
 
 bootstrap();
